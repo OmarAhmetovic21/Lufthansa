2eb6ac7e69a5dd6613239fcac1866448cb01a5c2
OmarAhmetovic21 <92754632+OmarAhmetovic21@users.noreply.github.com>
2022-09-16 14:39:47 +0200

    build

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,8 +6,6 @@ import { AllLoungesComponent } from './all-lounges/all-lounges.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignupComponent } from './signup/signup.component';
-import { LandingComponent } from './landing/landing.component';
-import { LoginComponent } from './login/login.component';
 import { AllAircraftComponent } from './all-aircraft/all-aircraft.component';
 import { CityCodesComponent } from './city-codes/city-codes.component';
 
@@ -15,8 +13,6 @@ const routes: Routes =[
     { path: 'home',             component: HomeComponent },
     { path: 'user-profile',     component: ProfileComponent },
     { path: 'register',           component: SignupComponent },
-    { path: 'landing',          component: LandingComponent },
-    { path: 'login',          component: LoginComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'all-aircraft', component: AllAircraftComponent},
     { path: 'city-codes', component: CityCodesComponent},
